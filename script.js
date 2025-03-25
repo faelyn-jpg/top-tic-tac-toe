c5fb81582ecffc51047cb8957dd413f83112df26
faelyn-jpg <169906179+faelyn-jpg@users.noreply.github.com>
2025-03-25 13:21:42 +1300

    psudocode for game logic, for console

---
+++ script.js
@@ -0,0 +1,47 @@
+//gameboard object, represents the state of the board
+//each square holds a Cell,
+//needs to method to add Cells to squares on board
+
+//gameboard - has rows, columns and board array
+//create 2d array for game board
+
+//getBoard method gets board for UI to render
+
+//addMarker method,
+//check if cell is available
+//if not available, stop execution,
+//otherwise add marker to cell for player
+
+//printBoard to see in console?
+
+//return methods to be used
+
+//Cell function
+//needs to accept players marker
+//needs to get value of cell to return
+
+//return token and value
+
+//game controller object
+//define player names
+//define board
+
+//player objects in array
+
+//set currently active player
+
+//need to be able to switch players
+
+//get the active player
+
+//print new round for who's turn it is
+
+//play round,
+//let player add marker
+//check for winner
+//switch who's turn it is
+//display whose turn it is
+
+//gamecontroller returns ability to play round and see who the active player is
+
+//set game to game controller
